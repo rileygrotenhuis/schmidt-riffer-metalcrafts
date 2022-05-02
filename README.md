3a5f520249b1b7478ab8444d9d22b14ea6f2ef8a
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-05-02 16:25:59 -0500

    feat: setup basic node.js application.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# schmidt-riffer-metalcrafts
-Schmidt-Riffer Metalcrafts Website
+# Schmidt-Riffer Metalcrafts
---
+++ src/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json('Schmidt-Riffer Metalcrafts');
+});
+
+app.listen(PORT, () => {
+    console.log(`Schdmit-Riffer Metalcrafts is running at http://localhost:${PORT}`);
+});
