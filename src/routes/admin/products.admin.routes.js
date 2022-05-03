8b09f4d376a5b58b708125edd1688a6f8d680257
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-05-02 22:31:31 -0500

    feat: add endpoints for creating a new product.

--- src/routes/admin/products.admin.routes.js
+++ src/routes/admin/products.admin.routes.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/products');
+const { PrismaClient } = require('@prisma/client');
+
+router.post('/products', async (req, res) => {
+    try {
+        const { name, description, image_url } = req.body;
+
+        if (!name) {
+            return res.status(400).json('All input is required');
+        }
+
+        const prisma = new PrismaClient();
+
+        const newProduct = await prisma.products.create({
+            data: {
+                name: name,
+                description: description,
+                image_url: image_url
+            }
+        });
+
+        prisma.$disconnect();
+
+        return res.status(201).json({
+            'data': newProduct
+        });
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+});
+
 router.put('/products/:id');
 router.delete('/products/:id');
 router.get('/products');
