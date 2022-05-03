35f4fa1e5bc0fb7eb6640d764a2a0304432a5fdc
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-05-02 22:15:57 -0500

    feat: create endpoints and logic for customer to get products list.

--- src/routes/public/index.public.routes.js
+++ src/routes/public/index.public.routes.js
@@ -3,8 +3,45 @@ const router = express.Router();
 
 const { PrismaClient } = require('@prisma/client');
 
-router.get('/products');
-router.get('/products/:id');
+router.get('/products', async (req, res) => {
+    try {
+        const prisma = new PrismaClient();
+
+        const products = await prisma.products.findMany();
+
+        prisma.$disconnect();
+
+        return res.status(200).json({
+            'data': products
+        });
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+});
+
+
+router.get('/products/:id', async (req, res) => {
+    try {
+        const prisma = new PrismaClient();
+
+        const product = await prisma.products.findFirst({
+            where: {
+                id: {
+                    equals: parseInt(req.params.id)
+                }
+            }
+        });   
+        
+        prisma.$disconnect();
+
+        return res.status(200).json({
+            'data': product
+        });
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+});
+
 router.post('/quotes');
 
 router.post('/contact', async (req, res) => {
