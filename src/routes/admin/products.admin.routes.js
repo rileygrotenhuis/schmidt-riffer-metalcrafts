afff5a5bffa4598a92df4b4151c5eaf3206d4398
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-05-02 22:36:59 -0500

    feat: add logic and endpoints for updating and deleting products.

--- src/routes/admin/products.admin.routes.js
+++ src/routes/admin/products.admin.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, Prisma } = require('@prisma/client');
 
 router.post('/products', async (req, res) => {
     try {
@@ -31,8 +31,53 @@ router.post('/products', async (req, res) => {
     }
 });
 
-router.put('/products/:id');
-router.delete('/products/:id');
+router.put('/products/:id', async (req, res) => {
+    try {
+        const { name, description, image_url } = req.body;
+
+        if (!name) {
+            return res.status(400).json('All input is required');
+        }
+
+        const prisma = new PrismaClient();
+
+        const updatedProduct = await prisma.products.update({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            data: {
+                name: name,
+                description: description,
+                image_url: image_url
+            }
+        });
+
+        prisma.$disconnect();
+
+        return res.status(200).json({
+            'data': updatedProduct
+        });
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+});
+
+router.delete('/products/:id', async (req, res) => {
+    try {
+        const prisma = new PrismaClient();
+
+        await prisma.products.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+
+        return res.status(204).json();
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+});
+
 router.get('/products');
 router.get('/products/:id');
 
